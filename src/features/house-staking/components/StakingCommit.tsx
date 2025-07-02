1be85e456b8d792aa5d1f0ed9534d21f9eefecb0
Strab614 <strab614@outlook.com>
2025-07-01 21:53:40 -0400

    Updated StakingCommit.tsx

--- src/features/house-staking/components/StakingCommit.tsx
+++ src/features/house-staking/components/StakingCommit.tsx
@@ -858,7 +858,7 @@ export const StakingCommit = ({
                         </Button>
                       </InputAdornment>
                     ),
-                  )}
+                  }}
                 />
                 <TextField
                   label="xALGO/chip"
